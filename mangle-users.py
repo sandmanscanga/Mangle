889a7d655037d59b7780c729b779ddbbf1f1f797
Adam Scanga <sandmanscanga@gmail.com>
2022-04-21 20:44:08 -0400

    Added a script used for mangling usernames based on an input file with users in the format of 'Firstname Lastname'

---
+++ mangle-users.py
@@ -0,0 +1,136 @@
+"""Module used to mangle username wordlists"""
+import random
+
+
+def mangle(name1, name2, domain=None):
+    """Mangle a pair of names"""
+
+    mangled = []
+
+    mangled.append(f"{name1}{name2}")
+    mangled.append(f"{name1}.{name2}")
+    if domain is not None:
+        mangled.append(f"{name1}{name2}@{domain}")
+        mangled.append(f"{name1}.{name2}@{domain}")
+
+    return mangled
+
+
+def mangle_combo(name1, name2, domain=None):
+    """Mangles a name combination excluding duplicate initials"""
+
+    mangled_combo = []
+    both_are_initials = len(name1) == 1 and len(name2) == 1
+    if both_are_initials is False:
+        mangled = mangle(name1, name2, domain=domain)
+        mangled_combo.extend(mangled)
+
+    return mangled_combo
+
+
+def get_name_combos(name, uppercase):
+    """Generates name combinations according to rules"""
+
+    combos = []
+    combos.append(name.lower())
+    combos.append(name.title())
+    combos.append(name[0].lower())
+    combos.append(name[0].upper())
+    if uppercase is True:
+        combos.append(name.upper())
+
+    return combos
+
+
+def mangle_bulk(name1, name2, uppercase, domain=None):
+    """Mangles all combinations of users"""
+
+    mangled_bulk = []
+    for name1_combo in get_name_combos(name1, uppercase):
+        for name2_combo in get_name_combos(name2, uppercase):
+            mangled = mangle_combo(name1_combo, name2_combo, domain=domain)
+            mangled_bulk.extend(mangled)
+
+    return mangled_bulk
+
+
+def main(args):
+    """Run the user mangler main process"""
+
+    # Load raw user names
+    with open(args.infile, "r") as file:
+        raw_names = file.read().strip().split("\n")
+
+    mangled = []
+    for raw_name in raw_names:
+        firstname, lastname = raw_name.split()
+        first_last_mangled = mangle_bulk(
+            firstname,
+            lastname,
+            args.uppercase,
+            domain=args.domain
+        )
+        last_first_mangled = mangle_bulk(
+            lastname,
+            firstname,
+            args.uppercase,
+            domain=args.domain
+        )
+        mangled.extend(first_last_mangled)
+        mangled.extend(last_first_mangled)
+
+    # Sort mangled results
+    if args.sort is True:
+        mangled = sorted(mangled)
+    elif args.random is True:
+        random.shuffle(mangled)
+
+    # Write mangled results to file
+    if args.outfile:
+        with open(args.outfile, "w") as file:
+            file.write("\n".join(mangled) + "\n")
+
+    # Display mangled results
+    if args.quiet is False:
+        for user in mangled:
+            print(user)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="specify input filename")
+    parser.add_argument(
+        "-d", "--domain",
+        required=False,
+        help="specify email domain to add as mangled suffix")
+    parser.add_argument(
+        "-u", "--uppercase",
+        action="store_true",
+        help="specify uppercase flag to include mangled uppercase users"
+    )
+    group1 = parser.add_mutually_exclusive_group()
+    group1.add_argument(
+        "-s", "--sort",
+        action="store_true",
+        help="specify sort flag to sort mangled users"
+    )
+    group1.add_argument(
+        "-r", "--random",
+        action="store_true",
+        help="specify random flag to randomize mangled users"
+    )
+    parser.add_argument(
+        "-o", "--outfile",
+        required=False,
+        help="specify output file to write mangled results"
+    )
+    parser.add_argument(
+        "-q", "--quiet",
+        action="store_true",
+        help="specify quiet flag to suppress output"
+    )
+    args = parser.parse_args()
+
+    main(args)
